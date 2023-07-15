6aca6f6f2825beb9286c329ef27de52cbcd49b60
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-07-15 13:12:43 +0100

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       },
       shared: ["react", "react-dom", "react-router-dom"],
     }),
+    VitePWA({ registerType: "autoUpdate" }),
   ],
   build: {
     modulePreload: false,
