d555e10548ca420b8ff637cb296263505cad6d7f
Isaac Etor <114070166+Ajinung@users.noreply.github.com>
2023-07-15 12:44:39 +0100

    changed everything

--- src/parts/instructions.tsx
+++ src/parts/instructions.tsx
@@ -9,7 +9,7 @@ const Instructions: React.FC<iInstruct> = ({ level, first, second, third }) => {
   return (
     <div className="w-11/12  md:mr-[30px] m-auto py-[20px] flex flex-col gap-[20px]">
       <div className="flex justify-between items-center">
-        <div className="text-[25px] font-[600]">Stars In Circles</div>
+        <div className="text-[25px] font-[800]">Learn Flex</div>
         <div className="py-1 px-3 rounded-md flex justify-center items-center bg-[#dbbc0d]">
           <div>
             Level <span className="font-[600]">{`${level}`}</span> 0f{" "}
