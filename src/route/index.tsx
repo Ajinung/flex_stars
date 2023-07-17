ad042db1759828cc4d7b115741cb7a4f7aa89f5a
victor-okereke <okerekev324@gmail.com>
2023-07-17 07:14:15 -0700

    completed congrats page

--- src/route/index.tsx
+++ src/route/index.tsx
@@ -125,7 +125,7 @@ export const router = createBrowserRouter([
     element: <TwentyFour />,
   },
   {
-    path: "/fin",
+    path: "/finfinfinfinfinfinfinfin",
     element: <Congrats />,
   },
 ]);
